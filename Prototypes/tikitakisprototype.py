ecf1c8c28f88bbb20ad4ba876764a5dc74ceb047
boi566 <84168449+boi566@users.noreply.github.com>
2026-09-20 12:34:30 +0530

    Add files via upload

---
+++ Prototypes/tikitakisprototype.py
@@ -0,0 +1,34 @@
+board = [" " , " " , " " ,
+" " , " " , " " ,
+" " , " " , " "]
+sub_board = [[0 , 1 , 2] , [3 , 4 , 5] , [6 , 7 , 8] , [0 , 3 , 6] , [2 , 5 , 8] , [1 , 4 , 7] , [2 , 4 , 6] , [0 , 4 , 8]]
+you = 'O'
+booty = 'X'
+curent = you
+while True:
+    game_over = False
+    try:
+        i = int(input("enter 0-8: "))
+        if board[i] == " ":
+            board[i] = curent
+            for i in range(1):
+                print(f"\033[H\033[2J{board[0:3]}\n{'-' * 15}\n{board[3:6]}\n{'-' * 15}\n{board[6:9]}")
+            if " " not in board:
+                print("its a draw")
+                break
+            for von in sub_board:
+                if board[von[0]] == board[von[1]] == board[von[2]] and board[von[0]] != " ":
+                    game_over = True
+            if game_over == True:
+                print("we got a winner")
+                break
+            if curent == you:
+                curent = booty
+            else:
+                curent = you
+        else:
+            print("spot taken bitch lolol")
+    except ValueError:
+        print("try again")
+    except IndexError:
+        print("0-8 only")
